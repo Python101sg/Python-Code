4d54d47172d18cb9e2ba761a28f94b77ebe9068a
P-Thangatharun <58984722+P-Thangatharun@users.noreply.github.com>
2020-01-20 22:22:47 +0800

    Update withdrawal.py

--- withdrawal.py
+++ withdrawal.py
@@ -1,21 +1,5 @@
 
-# Form implementation generated from reading ui file 'Withdrawal.ui'
-
-#
-
-# Created by: PyQt5 UI code generator 5.11.3
-
-#
-
-# WARNING! All changes made in this file will be lost!
-
-
-
-
 from PyQt5 import QtCore, QtGui, QtWidgets
-
-
-
 
 
 
